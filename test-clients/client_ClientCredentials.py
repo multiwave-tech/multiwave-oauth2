2c00a89c144be26bdf470c85eab82d6a9b9c994b
Samuel FORESTIER <sam.forestier@gmail.com>
2017-11-22 10:59:38 -0500

    Externalizes configuration for ClientCredentials test

--- test-clients/client_ClientCredentials.py
+++ test-clients/client_ClientCredentials.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 
 
+import json
 import requests
 
 
 if __name__ == '__main__':
 
+    # Retrieve configuration from server's sources for our tests
+    with open('oauth2-server/config/config.json') as file:
+        config = json.load(file)
+
     # Simply making a POST request on `/token` to retrieve one
     res = requests.post(
-        'http://localhost:8080/token',
+        'http://{0}:{1}/{2}'.format(
+            config['auth_server']['host'],
+            config['auth_server']['port'],
+            'token'
+        ),
         data={
             'grant_type': 'client_credentials',
-            'client_id': 'abc',
-            'client_secret': 'xyz'
+            'client_id': config['clients'][0]['client_id'],
+            'client_secret': config['clients'][0]['client_secret']
         }
     )
     res.raise_for_status()
